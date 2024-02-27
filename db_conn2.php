ff723692f691ead9a260d3c773621ef3bfbc932a
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-02-28 02:03:42 +0900

    mypage php 연결

---
+++ db_conn2.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+$id = 'root';
+$pass='1234';
+$db = 'huecandoitdb';
+$port = '3306';
+
+$conn = mysqli_connect($host, $id, $pass, $db, $port);
+
+?>
+
+<!-- http://127.0.0.1/hue-can-do-it-server/db_conn2.php -->
