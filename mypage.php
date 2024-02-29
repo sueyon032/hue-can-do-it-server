aafbe2c859e8ed128d56c7137637abbfefc4175f
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-03-01 03:50:39 +0900

    테이블 조인하여 id가 일치하는 레코드의 이름 가져오기

--- mypage.php
+++ mypage.php
@@ -1,18 +1,20 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/mypage.css">
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <title>휴캔두잇</title>
 </head>
+
 <body>
 
     <header>
@@ -27,15 +29,27 @@
     </header>
 
     <main>
-        <div class="info">
-            <div class="profile">
-                <i class="bi bi-emoji-smile"></i>
-            </div>
-            <div class="name"><b>suyeon</b></div>
-        </div>
 
         <?php
-            include('db_conn2.php');
+        include('db_conn2.php');
+
+        // user_info 테이블과 user_data 테이블을 조인하여 id가 일치하는 레코드의 이름을 가져오는 쿼리
+        $sql = "SELECT user_info.name 
+        FROM user_info
+        INNER JOIN user_data ON user_info.id = user_data.id";
+
+        $result = mysqli_query($conn, $sql);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="info">';
+            echo '<div class="profile">';
+            echo '<i class="bi bi-emoji-smile"></i>';
+            echo '</div>';
+            echo '<div class="name"><b>' . $row['name'] . '</b></div>';
+            echo '</div>';
+        }
+
+        mysqli_close($conn);
         ?>
 
         <div class="hr-container">
@@ -43,7 +57,7 @@
         </div>
 
         <div id="certificates-item">
-            
+
             <div class="cer-box-container">
                 <div class="cer-box">
                     <p class="cer-title">수료증</p>
@@ -125,6 +139,7 @@
     </main>
 
     <footer></footer>
-    
+
 </body>
+
 </html>
